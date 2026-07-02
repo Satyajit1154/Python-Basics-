1d582a145b93f9982785c55ddffb8b6a9cc33a6c
Satyajit1154 <jadhavsatyajeet11@gmail.com>
2026-07-02 23:14:13 +0530

    We have discussed more about variables here

---
+++ variables.py
@@ -0,0 +1,16 @@
+age =32
+height=6.11
+name="Satyajit"
+is_student=True
+
+print("age: ",age)
+print("Height: ",height)
+print("Name: ",name)
+
+print(type(age))
+
+int(height)
+print(height)
+
+age=int(input("What is the age"))
+print(age,type(age))
