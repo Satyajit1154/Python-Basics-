e61e75338b03af1d7263e42919217ed745138da1
Satyajit1154 <jadhavsatyajeet11@gmail.com>
2026-07-02 21:57:32 +0530

    Day1 changes

--- hello.py
+++ hello.py
@@ -1,2 +1,12 @@
 print("Hello, GitHub!")
 print("My AI Journey Starts Today 🚀")
+
+##this is a single line comment
+
+name="Satyajit"
+Name="Jadhav"
+print(name)
+print(Name)
+
+age =32
+print(type(age))
